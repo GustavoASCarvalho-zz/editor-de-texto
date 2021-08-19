4396ac47ddb446ebcef0ceea30a53ac6901caad8
Gustavo Alexandre da Silva Carvalho <gustavoalexandrescarvalho@gmail.com>
2021-08-19 20:51:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,31 @@
+const textarea = document.querySelector("textarea");
+const li = document.querySelectorAll("li");
+
+console.log(li);
+
+const text = [];
+
+textarea.addEventListener("input", function () {
+  textarea.style.height = "100%";
+  textarea.style.height = textarea.scrollHeight + "px";
+});
+
+li.forEach((element) => {
+  element.addEventListener("click", function () {
+    console.log(textarea.value);
+    if (getSelectText() !== "" || getSelectText() !== " ") {
+      const textBold = getSelectText();
+      textBold.style.fontWeight = "bold";
+      textarea.value =
+        textarea.value.substring(0, textarea.selectionStart) +
+        `${getSelectText()}` +
+        textarea.value.substring(textarea.selectionEnd);
+    }
+  });
+});
+
+getSelectText = () => {
+  var start = textarea.selectionStart;
+  var finish = textarea.selectionEnd;
+  return textarea.value.substring(start, finish);
+};
